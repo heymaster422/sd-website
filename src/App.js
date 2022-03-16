7b0c297c88928ebea102ac12c1abf143dd9109df
Andrew Wong <andrew.w14g@gmail.com>
2022-03-16 16:15:08 -0700

    created footer and added skills details.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Demo from './components/Pages/Demo';
 import Home from './components/Pages/Home';
 import Nav from './components/Nav/Nav';
 import Team from './components/Pages/Team';
-
+import Footer from './components/Nav/Footer';
 
 // create App Dark theme
 function App() {
@@ -15,13 +15,16 @@ function App() {
     
   <Router>
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route exact path="/"exact element={<Home />}/>
-        <Route path="Demo" element={<Demo />} />
-        <Route path="Gallery" element={<Gallery />} />
-        <Route path="Team" element={<Team />} />
-      </Routes>
+      <div className='content-wrap'>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="Demo" element={<Demo />} />
+          <Route path="Gallery" element={<Gallery />} />
+          <Route path="Team" element={<Team />} />
+        </Routes>
+        </div>
+      <Footer />
     </div>
   </Router>
 
